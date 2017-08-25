1ed127bb95d8096555e88c2ace349e725b4c172a
eouia <eouia0819@gmail.com>
2017-08-25 18:28:27 +0200

    MMM-TelegramBot supports

--- MMM-CalendarExt.js
+++ MMM-CalendarExt.js
@@ -2,8 +2,6 @@
 //@TODO I should make refactoring, there are too many garbage codes and duplicates.
 //
 
-
-
 const SELFNAME = 'calext'
 
 Module.register("MMM-CalendarExt", {
@@ -19,10 +17,150 @@ Module.register("MMM-CalendarExt", {
     return ["font-awesome.css", "custom.css", "preset.css"]
   },
 
+  getCommands: function(register) {
+    register.add({
+      command: 'whennext',
+      description : 'Show next event has title. You can try like this; `/whennext birthday`',
+      callback : 'TLGBOT_skd',
+      args_pattern : [/[^\s]+/i],
+      args_mapping : ["title"]
+    })
+    register.add({
+      command: 'skd',
+      description : 'Find upcoming schedules with specific date, calendar name and profile.\nSee more help with `/skd`',
+      callback : 'TLGBOT_skd',
+      args_pattern: [/d:[^ ,]+/i, /c:[^ ,]+/, /p:[^ ,]+/, /n:[0-9]+/],
+      args_mapping: ["date", "calendar", "profile", "count"]
+    })
+  },
+
+  TLGBOT_skd: function (command, handler) {
+    var helptext = "Find upcoming schedules with specific date, calendar name and profile.\n"
+      + "You can use these parameters seperated by space;\n"
+      + "`n:` - counts of events\n"
+      + "   maximum 10\n"
+      + "   default : 5\n"
+      + "`c:` - name of calendar\n"
+      + "`p:` - profile of calendar\n"
+      + "`d:` - period of calendar\n"
+      + "   today, tomorrow, thisw, nextw, thism, nextm\n"
+      + "   sun, mon, tue, wed, thu, fri, sat\n"
+      + "   default: today\n"
+      + "e.g. `/skd p:John d:nextm`\n"
+    var text = ""
+    var filter = {
+      profiles: [],
+      names: [],
+      titlePattern: "",
+      from: moment().startOf('day').format('x'),
+      to: moment().endOf('day').format('x'),
+      count: 5
+    }
+    if (command == 'whennext') {
+      helptext = "You should use this command with title text like this; `/whennext birthday`"
+    }
+    var args = handler.args
+    if (args == null) {
+      handler.reply("TEXT", helptext, {parse_mode:'Markdown'})
+      return
+    }
+    var date = (args.date) ? args.date.substring(2) : null
+    var calendar = (args.calendar) ? [args.calendar.substring(2)] : []
+    var profile = (args.profile) ? [args.profile.substring(2)] : []
+    var count = (args.counts) ? args.counts.substring(2) : 5
+    filter.profile = profile
+    filter.name = calendar
+    filter.count = count
+    var todayN = moment().isoWeekday()
+    var dayN
+    switch (date) {
+      case 'tomorrow':
+        filter.from = moment().add(1, 'days').startOf('day').format('x')
+        filter.to = moment().add(1, 'days').endOf('day').format('x')
+        break;
+      case 'thisw':
+        filter.from = moment().startOf('week').format('x')
+        filter.to = moment().endOf('week').format('x')
+        break;
+      case 'nextw':
+        filter.from = moment().add(1, 'weeks').startOf('week').format('x')
+        filter.to = moment().add(1, 'weeks').endOf('week').format('x')
+        break;
+      case 'thism':
+        filter.from = moment().startOf('month').format('x')
+        filter.to = moment().endOf('month').format('x')
+        break;
+      case 'nextm':
+        filter.from = moment().add(1, 'months').startOf('week').format('x')
+        filter.to = moment().add(1, 'months').endOf('week').format('x')
+        break;
+      case 'mon':
+        dayN = (todayN >= 1) ? 8 : 1
+        filter.from = moment().isoWeekday(dayN).startOf('day').format('x')
+        filter.to = moment().isoWeekday(dayN).endOf('day').format('x')
+        break;
+      case 'tue':
+        dayN = (todayN >= 2) ? 9 : 2
+        filter.from = moment().isoWeekday(dayN).startOf('day').format('x')
+        filter.to = moment().isoWeekday(dayN).endOf('day').format('x')
+        break;
+      case 'wed':
+        dayN = (todayN >= 3) ? 10 : 3
+        filter.from = moment().isoWeekday(dayN).startOf('day').format('x')
+        filter.to = moment().isoWeekday(dayN).endOf('day').format('x')
+        break;
+      case 'thu':
+        dayN = (todayN >= 4) ? 11 : 4
+        filter.from = moment().isoWeekday(dayN).startOf('day').format('x')
+        filter.to = moment().isoWeekday(dayN).endOf('day').format('x')
+        break;
+      case 'fri':
+        dayN = (todayN >= 5) ? 12 : 5
+        filter.from = moment().isoWeekday(dayN).startOf('day').format('x')
+        filter.to = moment().isoWeekday(dayN).endOf('day').format('x')
+        break;
+      case 'sat':
+        dayN = (todayN >= 6) ? 13 : 6
+        filter.from = moment().isoWeekday(dayN).startOf('day').format('x')
+        filter.to = moment().isoWeekday(dayN).endOf('day').format('x')
+        break;
+      case 'sun':
+        dayN = (todayN >= 7) ? 14 : 7
+        filter.from = moment().isoWeekday(dayN).startOf('day').format('x')
+        filter.to = moment().isoWeekday(dayN).endOf('day').format('x')
+        break;
+    }
+    if (command == 'whennext') {
+      filter.to = moment().add(6, 'months').endOf('week').format('x')
+      filter.titlePattern = (args.title) ? args.title : ""
+      filter.count = 1
+    }
+    var events = this.findSchedule(filter, null, null)
+    if (events.events.length <= 0) {
+      text = "I cannot find any schedule which you described. Try another."
+      handler.reply("TEXT", text)
+    } else {
+      var sd = (events.events.length == 1) ? 1 : 0
+      events.events.forEach((e)=> {
+        var title = (e.title) ? e.title : " "
+        var location = (e.location) ? e.location : " "
+        var description = (e.description && sd) ? e.description : " "
+        var time = (e.startDate) ? moment(parseInt(e.startDate)).format("MMM D, HH:mm:ss") : " "
+        text += "*" + title + "*\n"
+        text += "`" + time + "`\n"
+        text += "_" + location + "_\n\n"
+        text += description + "\n"
+      })
+      handler.reply("TEXT", text, {parse_mode:'Markdown'})
+    }
+  },
+
   start: function() {
     this.observer = null
     this.isInit = 0
     this.redrawTimer = null
+    this.profileSet = new Set()
+    this.nameSet = new Set()
   },
 
   suspend: function() {
@@ -30,7 +168,6 @@ Module.register("MMM-CalendarExt", {
       R.hide()
       this.draw()
     }
-
   },
 
   resume: function() {
@@ -50,7 +187,6 @@ Module.register("MMM-CalendarExt", {
 
   resetCalendars: function() {
     this.events=[];
-    //Slots.resetSlots();
     this.sendSocketNotification("RESET_CALENDARS")
   },
 
@@ -69,26 +205,23 @@ Module.register("MMM-CalendarExt", {
   getDom: function() {
     if (this.isInit) {
       R = new Render ()
-
       R.drawViews(
         this.CurrentConfigs,
         this.getEventsToDraw()
       )
     }
-
-
     var wrapper = null
     wrapper = document.createElement("div")
     wrapper.id = 'CALEXT_proxy'
     wrapper.className = 'proxy'
     return wrapper
   },
+
   getEventsToDraw: function() {
     var locale = this.CurrentConfigs.system.locale
     var profile = this.profile
     var views = this.CurrentConfigs.system.show
     var oldLimit = moment().locale(locale).add(-1,'days').endOf('day')
-
     var self = this
     this.instantEvents = this.instantEvents.filter(function (e) {
       var uid = e.uid.split('@')[0]
@@ -109,7 +242,6 @@ Module.register("MMM-CalendarExt", {
     eArr = eArr.concat(this.events, this.instantEvents)
     var isForAnyone = (self.profile == null || self.profile == '') ? 1 : 0
     var isForAnyView = (views.length > 0) ? 0 : 1
-
     if (!isForAnyone) {
       eArr = eArr.filter(function(e) {
         if (e.profiles.length > 0) { // is this event dedicated to some profiles?
@@ -139,12 +271,9 @@ Module.register("MMM-CalendarExt", {
         }
       })
     }
-
-
     eArr.sort(function(a, b) {
       return a - b
     })
-
     return eArr
   },
 
@@ -186,14 +315,12 @@ Module.register("MMM-CalendarExt", {
       sessionId : sessionId,
       sender : senderName
     }
-
     if(this.instantEvents.filter(function (e) {
       return ev.sender == senderName
     }).length >= 100) {
       msg.message = 'TOO_MANY_EVENTS'
       this.sendNotification('CALEXT_SAYS_ADD_EVENT_RESULT', msg)
     }
-
     var curCfg = this.CurrentConfigs
     if(typeof curCfg == 'undefined') {
       msg.uid = ev.uid
@@ -220,7 +347,6 @@ Module.register("MMM-CalendarExt", {
       endDate: moment().valueOf(),
       fullDayEvent: 0,
     }
-
     for(var param in eDefault) {
       if (typeof ev[param] === 'undefined') {
         ev[param] = eDefault[param]
@@ -230,8 +356,6 @@ Module.register("MMM-CalendarExt", {
     msg.uid = tuid
     ev.uid = (tuid + '@' + senderName).toString()
     ev.sender = senderName
-
-
     var eArr = this.instantEvents.filter(function (e) {
       return (e.sender == senderName) && (e.uid == ev.uid)
     })
@@ -267,24 +391,22 @@ Module.register("MMM-CalendarExt", {
     }
   },
 
-  saySchedule(filterOrigin, senderName, sessionId) {
-    var filter ={}
-    var filterDefault = {
-      profiles: [],
-      names: [],
-      titlePattern: "",
-      from: moment().format('x'),
-      to: moment().add(2, 'days').format('x'),
-      count: 10
-    }
-
-    for(var param in filterDefault) {
-      if (typeof filterOrigin[param] === 'undefined') {
-        filter[param] = filterDefault[param]
-      } else {
-        filter[param] = filterOrigin[param]
+  makeIndex: function() {
+    var events = this.events.concat(this.instantEvents)
+    var profileSet = new Set()
+    var nameSet = new Set()
+    events.forEach((e)=>{
+      if(e.profiles.length > 0) {
+        var epf = new Set(e.profiles)
+        profileSet = new Set([...profielSet, ...epf])
       }
-    }
+      if(e.name) nameSet.add(e.name)
+    })
+    this.profileSet = profileSet
+    this.nameSet = nameSet
+  },
+
+  findSchedule(filter, senderName, sessionId) {
     var events = this.events.concat(this.instantEvents)
     var final = events.filter(function(e){
       if (filter.profiles.length > 0) {
@@ -311,7 +433,6 @@ Module.register("MMM-CalendarExt", {
           return 0
         }
       }
-
       if (
         moment(parseInt(e.startDate)).isBefore(moment(parseInt(filter.from)))
       ) {
@@ -322,7 +443,7 @@ Module.register("MMM-CalendarExt", {
       ) {
         return 0
       }
-      if(filter.titlePattern !== "") {
+      if(filter.titlePattern) {
         var pattern = filter.titlePattern
         var r = (pattern instanceof RegExp) ? pattern : pattern.toRegexp()
         if (!e.title.match(r)) return 0;
@@ -348,7 +469,7 @@ Module.register("MMM-CalendarExt", {
       message : "",
       sessionId : sessionId,
       sender : senderName,
-      filter: filterOrigin
+      filter: filter
     }
 
     if (final.length > 0) {
@@ -358,32 +479,34 @@ Module.register("MMM-CalendarExt", {
       msg.message = "SCHEDULE_NOTFOUND"
       msg.events = []
     }
-
-    this.sendNotification('CALEXT_SAYS_SCHEDULE', msg)
+    return msg
   },
-/*
-  modifyConfiguration: function(newCfg, senderName, sessionId) {
-    this.CurrentConfigs = this.CurrentConfigs.modify(newCfg)
-    var msg = {
-      sessionId : sessionId,
-      sender : senderName,
+
+  saySchedule(filterOrigin, senderName, sessionId) {
+    var filter ={}
+    var filterDefault = {
+      profiles: [],
+      names: [],
+      titlePattern: "",
+      from: moment().format('x'),
+      to: moment().add(2, 'days').format('x'),
+      count: 10
     }
-    if (this.CurrentConfigs.system.useProfileConfig) {
-      if (typeof this.CurrentConfigs.profileConfigs !== 'undefined') {
-        if (typeof this.CurrentConfigs.profileConfigs[this.profile] !== 'undefined') {
-          this.CurrentConfigs
-            = new Configs().make(this.CurrentConfigs.profileConfigs[this.profile])
-        }
+
+    for(var param in filterDefault) {
+      if (typeof filterOrigin[param] === 'undefined') {
+        filter[param] = filterDefault[param]
+      } else {
+        filter[param] = filterOrigin[param]
       }
     }
 
+    var msg = this.findSchedule(filter, senderName, sessionId)
 
-    this.events = []
-    this.resetCalendars()
-    this.draw()
-    this.sendNotification('CALEXT_SAYS_CONFIG_MODIFIED', msg)
+
+    this.sendNotification('CALEXT_SAYS_SCHEDULE', msg)
   },
-*/
+
   notificationReceived: function(notification, payload, sender) {
     var sessionId = moment().valueOf()
     if (typeof payload !== 'undefined') {
@@ -391,7 +514,6 @@ Module.register("MMM-CalendarExt", {
         sessionId = payload.sessionId
       }
     }
-
     switch (notification) {
       case 'DOM_OBJECTS_CREATED':
         if(typeof sender == 'undefined') {
@@ -422,7 +544,6 @@ Module.register("MMM-CalendarExt", {
         break
       case 'CALEXT_MODIFY_CONFIG':
         this.modifyConfiguration(payload.config, sender.name, sessionId)
-        //this.CurrentConfigs.modify(payload)
     }
   },
 
@@ -430,6 +551,7 @@ Module.register("MMM-CalendarExt", {
     switch (notification) {
       case 'CALENDAR_MODIFIED':
         this.events = payload
+        this.makeIndex()
         this.draw()
         break
       case 'READY_TO_ADD_CALENDAR':
@@ -455,7 +577,6 @@ Module.register("MMM-CalendarExt", {
     } else {
       needReloadCalendar = 0
     }
-
     if(typeof newCfg.calendars !== 'undefined' && newCfg.calendars.length > 0) {
       needReloadCalendar = 1
     }
@@ -472,7 +593,6 @@ Module.register("MMM-CalendarExt", {
   initAfterLoading: function(profile="") {
     var self = this
     var needReloadCalendar = 1
-
     this.CurrentConfigs = new Configs().make(this.config)
     //if(profile == 'User2') debugger;
     if(!profile) {
@@ -492,17 +612,14 @@ Module.register("MMM-CalendarExt", {
         needReloadCalendar = 0
       }
     }
-
     if(needReloadCalendar) {
       this.events = []
       this.resetCalendars()
       this.sendNotification('CALEXT_SAYS_READY', null)
     }
-
     this.isInit = 1
-
-
   },
+
   loadCSS: function() {
     var css = [
       {
@@ -541,15 +658,11 @@ Module.register("MMM-CalendarExt", {
         return
       }
     }
-
-    //if(!this.showing) return
-    //this.fetchEvents()
     this.updateDom()
     var self = this
     clearInterval(this.redrawTimer)
     this.redrawTimer = null
     var redrawInterval = null
-
     if (typeof this.CurrentConfigs !== 'undefined') {
       redrawInterval = this.CurrentConfigs.system.redrawInterval
     }
@@ -560,7 +673,6 @@ Module.register("MMM-CalendarExt", {
       self.draw()
     }, redrawInterval)
   },
-
 })
 
 function sleep(ms) {
